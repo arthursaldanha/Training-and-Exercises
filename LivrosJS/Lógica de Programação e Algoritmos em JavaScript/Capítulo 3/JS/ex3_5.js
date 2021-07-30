0021fe136386a8cebe88513659984a93eeccc08a
Arthur Saldanha <saldanhadev@gmail.com>
2021-07-30 14:21:40 -0300

    Exercicios 3.5

---
+++ LivrosJS/Lógica de Programação e Algoritmos em JavaScript/Capítulo 3/JS/ex3_5.js
@@ -0,0 +1,26 @@
+(() => {
+   function calcularRaiz() {
+      var inNumero = document.querySelector("#inNumero");
+      var outResposta = document.querySelector("#outResposta");
+
+      var numero = Number(inNumero.value);
+
+      if (numero == 0 || isNaN(numero)) {
+         alert("Por favor, informe um número inteiro positivo...");
+         inNumero.focus();
+         return;
+      }
+
+      var raiz = Math.sqrt(numero);
+
+      if (raiz == Math.floor(raiz)) {
+         outResposta.textContent = "Raiz: " + raiz;
+      } else {
+         outResposta.textContent = "Não há raiz exata para o número " + numero;
+      }
+   }
+
+   var btExibir = document.querySelector("#btExibir");
+   btExibir.addEventListener("click", calcularRaiz);
+
+})();
